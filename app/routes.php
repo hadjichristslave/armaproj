ea59283093d870f9735fd4a0ad91b8a9fbfb8b74
Slave <p.hadjichristodoulou@hotmail.com>
2014-02-01 00:15:24 +0100

    asdf

--- app/routes.php
+++ app/routes.php
@@ -16,10 +16,10 @@ Route::get('/', function()
 	var_dump(Geographcompartment::find(1)->counties);
 	echo '-----------------------------';
 	var_dump(County::find(1)->compartment);
-	var_dump(County::find(1)->town;
+	var_dump(County::find(1)->town);
 	echo '-----------------------------';
-	var_dump(Town::find(1)->county;
-	var_dump(Town::find(1)->area;
+	var_dump(Town::find(1)->county);
+	var_dump(Town::find(1)->area);
 	echo '-----------------------------';
 	var_dump(Area::find(1)->town);
 	return View::make('hello');
