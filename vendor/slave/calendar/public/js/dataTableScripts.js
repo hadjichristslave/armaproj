f30e0584115f619f07ff314ea528515b2d167b53
Slave <p.hadjichristodoulou@hotmail.com>
2014-02-02 20:19:44 +0100

    asdf

--- vendor/slave/calendar/public/js/dataTableScripts.js
+++ vendor/slave/calendar/public/js/dataTableScripts.js
@@ -5,12 +5,15 @@
     $(document).ready(function() {
          if( $.fn.select2 ) {
             $("select.mws-select2").select2(data: {
-    results: [
+    
       {title: "My shiny group", children: [
           {id: 1, title: "My shiny item"}, 
           {id: 2, title: "My shiny item2"}
       ]}
-    ],
+    ,{title: "My shiny group 2", children: [
+          {id: 3, title: "My shiny item"}, 
+          {id: 4, title: "My shiny item2"}
+      ]},
     text: "title");
         }
         $("#mws-form-dialog").dialog({
