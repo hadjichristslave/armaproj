00643c318b475589580ea356eb4eda7403d513e8
Slave <p.hadjichristodoulou@hotmail.com>
2014-02-02 20:37:20 +0100

    asdf

--- public/packages/slave/calendar/js/dataTableScripts.js
+++ public/packages/slave/calendar/js/dataTableScripts.js
@@ -5,6 +5,7 @@
     $(document).ready(function() {
          if( $.fn.select2 ) {
             $("select.mws-select2").select2();
+        }
         $("#mws-form-dialog").dialog({
                 autoOpen: false,
                 modal: true,
