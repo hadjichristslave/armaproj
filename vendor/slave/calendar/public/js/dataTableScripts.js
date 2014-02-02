6b166048676abd24a4562a3e79b6637b7dd048cb
Slave <p.hadjichristodoulou@hotmail.com>
2014-02-02 20:29:02 +0100

    asdf

--- vendor/slave/calendar/public/js/dataTableScripts.js
+++ vendor/slave/calendar/public/js/dataTableScripts.js
@@ -4,18 +4,7 @@
 
     $(document).ready(function() {
          if( $.fn.select2 ) {
-            $("select.mws-select2").select2(data: {
-    
-      {title: "My shiny group", children: [
-          {id: 1, title: "My shiny item"}, 
-          {id: 2, title: "My shiny item2"}
-      ]}
-    ,{title: "My shiny group 2", children: [
-          {id: 3, title: "My shiny item"}, 
-          {id: 4, title: "My shiny item2"}
-      ]},
-    text: "title");
-        }
+            $("select.mws-select2").select2();
         $("#mws-form-dialog").dialog({
                 autoOpen: false,
                 modal: true,
--- vendor/slave/calendar/src/views/calendar.blade.php
+++ vendor/slave/calendar/src/views/calendar.blade.php
@@ -70,13 +70,23 @@
 						</div>
 					</div>
 					<div class="mws-form-row">
-						<label class="mws-form-label">Last Modify From:</label>
+						<label class="mws-form-label">Διαθέσιμοι υπάληλοι για κλείσιμο:</label>
 							<select class="mws-select2 medium ev_creator_class" name="user_id">
+				               <optgroup label="0 φορές αυτό το μήνα">
+				                   <option value="3">Casper</option>
+				               </optgroup>
+				               <optgroup label="1 φορά μέσα στο μήνα">
+				                   <option value="2">homer</option>
+				               </optgroup>
+				               <optgroup label="κάθε μέρα του μήνα">
+				                   <option value="1">panos</option>
+				                   <option value="1">alex</option>
+				               </optgroup>
 							</select>
 					</div>
 					<div class="mws-form-row" style="display:none;">
 						<label class="mws-form-label">All day </label>
-						<div class="mws-form-item">
+						<div class="mws-form-item"> 
 							<input type="text" class="mws-dtpicker calendar_allday" name="allDay" readonly="readonly" />
 						</div>
 					</div>
