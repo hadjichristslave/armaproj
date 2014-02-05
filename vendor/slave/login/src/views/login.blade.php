fe7820afc71146ebf9c54e0e8090defc6fb0272f
Slave <p.hadjichristodoulou@hotmail.com>
2014-02-05 19:49:16 +0100

    asf

--- vendor/slave/login/src/views/login.blade.php
+++ vendor/slave/login/src/views/login.blade.php
@@ -107,7 +107,7 @@
 <!-- END LOGIN -->
 <!-- BEGIN COPYRIGHT -->
 <div class="copyright">
-	 2013 &copy; Metronic - Admin Dashboard Template.
+	 2014 &copy; 
 </div>
 <!-- END COPYRIGHT -->
 <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
