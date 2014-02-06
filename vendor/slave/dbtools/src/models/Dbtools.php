810dac1ce12677f190fc83d9c84f987832b405dd
Slave <p.hadjichristodoulou@hotmail.com>
2014-02-06 17:31:54 +0100

    asdf

--- vendor/slave/dbtools/src/models/Dbtools.php
+++ vendor/slave/dbtools/src/models/Dbtools.php
@@ -46,19 +46,22 @@ class Dbtools extends Model {
 	public static function updateFromModel($model , $id , $tablekey){
 		$input = Input::except('_token');
         $inputData = $model::where($tablekey, '=' , $id)->first();
+        die('there');
         try{
         	if(!Dbtools::exists($model, $id, $tablekey))
 				return "database-4";
-
+            die('no, there');
             foreach ($input as $key => $value)
                     $inputData->$key = $value;
 
             $flag = Validpack::validateoperation($inputData);
+            die('no, there there');
             if($flag->passes()){
                 foreach ($inputData->attributes as $key => $value)
                     if($key == "password")
                         $inputData->$key = Hash::make($value);
                 $inputData->save();
+                die('no, there there there');
                 return "database-1";
             }else{
                 return "database-2";
