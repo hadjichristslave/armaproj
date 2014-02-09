cad02b4967403cb8baca432ca995313c963102bd
Slave <p.hadjichristodoulou@hotmail.com>
2014-02-09 18:18:03 +0100

    adsf

--- public/scripts/customScripts.js
+++ public/scripts/customScripts.js
@@ -3,8 +3,8 @@ jQuery( document ).ready(function( $ ) {
   alert('this');
 	$( "#companyIdSelect" ).change(function() {
 		  $.get( "/azadmin/myproject/public/app/return/Store/"+$(this).val()+"/true", function(data) {
-		  	console.log(data.substring(0,-4));
-		  	var response = JSON.parse(data.substring(0,-4));
+		  	console.log(data.slice(0,-4));
+		  	var response = JSON.parse(data.slice(0,-4));
 		  	 $.each(response, function(index, element) {
 	            console.log(element);
 
