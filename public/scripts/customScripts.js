7d64a11674edd4c147a635e227d50997f04033aa
Slave <p.hadjichristodoulou@hotmail.com>
2014-02-09 17:59:28 +0100

    adsf

--- public/scripts/customScripts.js
+++ public/scripts/customScripts.js
@@ -3,7 +3,7 @@ jQuery( document ).ready(function( $ ) {
   alert('this');
 	$( "#companyIdSelect" ).change(function() {
 		  $.get( "/azadmin/myproject/public/app/return/Store/"+$(this).val()+"/true", function( data ) {
-		  	$.each(answer, function(dat){
+		  	$.each(data, function(dat){
 		  		console.log(dat);
 		  	});
 		});
