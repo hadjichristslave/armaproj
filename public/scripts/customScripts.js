00a280e4edd3f5bde1caa89f8075beccc6bffc02
Slave <p.hadjichristodoulou@hotmail.com>
2014-02-09 17:58:19 +0100

    adsf

--- public/scripts/customScripts.js
+++ public/scripts/customScripts.js
@@ -3,11 +3,8 @@ jQuery( document ).ready(function( $ ) {
   alert('this');
 	$( "#companyIdSelect" ).change(function() {
 		  $.get( "/azadmin/myproject/public/app/return/Store/"+$(this).val()+"/true", function( data ) {
-		  	alert( "Load was performed." );
-		  	var answer = eval('(' + data + ')');
-		  	$.each(answer, function(key,val){
-		  		console.log(key);
-		  		console.log(val);
+		  	$.each(answer, function(dat){
+		  		console.log(dat);
 		  	});
 		});
 	});
