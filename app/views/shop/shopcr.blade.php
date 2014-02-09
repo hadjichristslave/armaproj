fbfc6bf37fd84718776ac8a285fe3167bfac782a
Slave <p.hadjichristodoulou@hotmail.com>
2014-02-09 12:31:00 +0100

    asdf

--- app/views/shop/shopcr.blade.php
+++ app/views/shop/shopcr.blade.php
@@ -4,7 +4,7 @@
 				<div class="col-md-12">
 					<!-- BEGIN PAGE TITLE & BREADCRUMB-->
 					<h4 class="page-title">
-					Φόρμα Δημιουργίας καταστήματος<small> βασικές πληροφορίες</small>
+					Εταιρία<small> βασικές πληροφορίες</small>
 					</h4>
 					<!-- END PAGE TITLE & BREADCRUMB-->
 				</div>
@@ -13,7 +13,7 @@
 								<div class="portlet box green">
 									<div class="portlet-title">
 										<div class="caption">
-											<i class="fa fa-reorder"></i>Form Sample
+											<i class="fa fa-reorder"></i>Φορμα δημιουργίας
 										</div>
 										<div class="tools">
 											<a href="javascript:;" class="collapse"></a>
@@ -26,11 +26,23 @@
 										<!-- BEGIN FORM-->
 										<form action="#" class="form-horizontal">
 											<div class="form-body">
-												<h3 class="form-section">Person Info</h3>
+												<h3 class="form-section">Γενικές Πληροφορίες</h3>
 												<div class="row">
 													<div class="col-md-6">
 														<div class="form-group">
-															<label class="control-label col-md-3">First Name</label>
+															<label class="control-label col-md-3">Ονομασία</label>
+															<div class="col-md-9">
+																<input type="text" class="form-control" name="brand" placeholder="π.χ Azade">
+																<span class="help-block">
+																	Η ονομασία της εταιρίας
+																</span>
+															</div>
+														</div>
+													</div>
+													<!--/span-->
+													<!-- <div class="col-md-6">
+														<div class="form-group">
+															<label class="control-label col-md-3">Διακριτικός τίτλος</label>
 															<div class="col-md-9">
 																<input type="text" class="form-control" placeholder="Chee Kin">
 																<span class="help-block">
@@ -38,171 +50,56 @@
 																</span>
 															</div>
 														</div>
-													</div>
-													<!--/span-->
-													<div class="col-md-6">
-														<div class="form-group has-error">
-															<label class="control-label col-md-3">Last Name</label>
-															<div class="col-md-9">
-																<div class="select2-container select2me form-control" id="s2id_autogen1"><a href="javascript:void(0)" onclick="return false;" class="select2-choice" tabindex="-1">   <span class="select2-chosen">Abc</span><abbr class="select2-search-choice-close"></abbr>   <span class="select2-arrow"><b></b></span></a><input class="select2-focusser select2-offscreen" type="text" id="s2id_autogen2"><div class="select2-drop select2-display-none select2-with-searchbox">   <div class="select2-search">       <input type="text" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" class="select2-input">   </div>   <ul class="select2-results">   </ul></div></div><select name="foo" class="select2me form-control select2-offscreen" tabindex="-1">
-																	<option value="1">Abc</option>
-																	<option value="1">Abc</option>
-																	<option value="1">This is a really long value that breaks the fluid design for a select2</option>
-																</select>
-																<span class="help-block">
-																	This field has error.
-																</span>
-															</div>
-														</div>
-													</div>
-													<!--/span-->
+													</div> -->
 												</div>
+													
+												<h3 class="form-section">Γεωγραφικές πληροφορίες</h3>
 												<!--/row-->
 												<div class="row">
 													<div class="col-md-6">
 														<div class="form-group">
-															<label class="control-label col-md-3">First Name</label>
+															<label class="control-label col-md-3">Διεύθυνση</label>
 															<div class="col-md-9">
-																<input type="text" class="form-control" placeholder="Chee Kin">
-																<span class="help-block">
-																	This is inline help
-																</span>
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-													<div class="col-md-6">
-														<div class="form-group has-error">
-															<label class="control-label col-md-3">Last Name</label>
-															<div class="col-md-9">
-																<input type="text" class="form-control" placeholder="Lim">
-																<span class="help-block">
-																	This field has error.
-																</span>
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
-												<!--/row-->
-												<div class="row">
-													<div class="col-md-6">
-														<div class="form-group">
-															<label class="control-label col-md-3">Gender</label>
-															<div class="col-md-9">
-																<select class="form-control">
-																	<option value="">Male</option>
-																	<option value="">Female</option>
-																</select>
-																<span class="help-block">
-																	Select your gender.
-																</span>
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-													<div class="col-md-6">
-														<div class="form-group">
-															<label class="control-label col-md-3">Date of Birth</label>
-															<div class="col-md-9">
-																<input type="text" class="form-control" placeholder="dd/mm/yyyy">
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
-												<!--/row-->
-												<div class="row">
-													<div class="col-md-6">
-														<div class="form-group">
-															<label class="control-label col-md-3">Category</label>
-															<div class="col-md-9">
-																<div class="select2-container select2_category form-control" id="s2id_autogen5"><a href="javascript:void(0)" onclick="return false;" class="select2-choice" tabindex="-1">   <span class="select2-chosen">Category 1</span><abbr class="select2-search-choice-close"></abbr>   <span class="select2-arrow"><b></b></span></a><input class="select2-focusser select2-offscreen" type="text" id="s2id_autogen6" tabindex="1"><div class="select2-drop select2-display-none select2-with-searchbox">   <div class="select2-search">       <input type="text" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" class="select2-input">   </div>   <ul class="select2-results">   </ul></div></div><select class="select2_category form-control select2-offscreen" data-placeholder="Choose a Category" tabindex="-1">
-																	<option value="Category 1">Category 1</option>
-																	<option value="Category 2">Category 2</option>
-																	<option value="Category 3">Category 5</option>
-																	<option value="Category 4">Category 4</option>
-																</select>
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-													<div class="col-md-6">
-														<div class="form-group">
-															<label class="control-label col-md-3">Membership</label>
-															<div class="col-md-9">
-																<div class="radio-list">
-																	<label class="radio-inline">
-																	<div class="radio"><span><input type="radio" name="optionsRadios2" value="option1"></span></div>
-																	Free </label>
-																	<label class="radio-inline">
-																	<div class="radio"><span class="checked"><input type="radio" name="optionsRadios2" value="option2" checked=""></span></div>
-																	Professional </label>
-																</div>
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
-												<h3 class="form-section">Address</h3>
-												<!--/row-->
-												<div class="row">
-													<div class="col-md-6">
-														<div class="form-group">
-															<label class="control-label col-md-3">Address 1</label>
-															<div class="col-md-9">
-																<input type="text" class="form-control">
+																<input type="text" name="address" class="form-control">
 															</div>
 														</div>
 													</div>
 													<div class="col-md-6">
 														<div class="form-group">
-															<label class="control-label col-md-3">Address 2</label>
+															<label class="control-label col-md-3">Περιοχή</label>
 															<div class="col-md-9">
-																<input type="text" class="form-control">
+																<input type="text" name="area" class="form-control" placeholder="Περιοχή">
 															</div>
 														</div>
 													</div>
 												</div>
-												<div class="row">
-													<div class="col-md-6">
-														<div class="form-group">
-															<label class="control-label col-md-3">City</label>
-															<div class="col-md-9">
-																<input type="text" class="form-control">
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-													<div class="col-md-6">
-														<div class="form-group">
-															<label class="control-label col-md-3">State</label>
-															<div class="col-md-9">
-																<input type="text" class="form-control">
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
-												<!--/row-->
 												<div class="row">
 													<div class="col-md-6">
 														<div class="form-group">
 															<label class="control-label col-md-3">Post Code</label>
 															<div class="col-md-9">
-																<input type="text" class="form-control">
+																<input type="text" name='postcode' class="form-control">
 															</div>
 														</div>
 													</div>
 													<!--/span-->
 													<div class="col-md-6">
 														<div class="form-group">
-															<label class="control-label col-md-3">Country</label>
+															<label class="control-label col-md-3">Πόλη</label>
 															<div class="col-md-9">
-																<select class="form-control">
-																	<option>Country 1</option>
-																	<option>Country 2</option>
-																</select>
+																<input type="text" name="city" class="form-control" placeholder="Πόλη">
+															</div>
+														</div>
+													</div>
+													<!--/span-->
+												</div>
+												<!--/row-->
+												<div class="row">
+													<div class="col-md-6">
+														<div class="form-group">
+															<label class="control-label col-md-3">Νομός</label>
+															<div class="col-md-9">
+																<input type="text" name="county" class="form-control" placeholder="Νομός">
 															</div>
 														</div>
 													</div>
@@ -214,8 +111,8 @@
 												<div class="row">
 													<div class="col-md-6">
 														<div class="col-md-offset-3 col-md-9">
-															<button type="submit" class="btn green">Submit</button>
-															<button type="button" class="btn default">Cancel</button>
+															<button type="submit" class="btn green">Υποβολή</button>
+															<button type="button" class="btn default" id="fieldsReset">Εκαθάρηση πεδίων</button>
 														</div>
 													</div>
 													<div class="col-md-6">
