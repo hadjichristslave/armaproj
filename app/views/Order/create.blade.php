dae5502277c6a14adb7f8418c5731b7f333eda53
Slave <p.hadjichristodoulou@hotmail.com>
2014-02-16 14:16:44 +0100

    asdf

--- app/views/Order/create.blade.php
+++ app/views/Order/create.blade.php
@@ -85,6 +85,13 @@
 						<div class="col-md-6">
 							<div class="col-md-offset-3 col-md-9">
 								<button type="submit" class="btn green">Αποθήκευση Παραγγελίας</button>
+								<label class="control-label col-md-3">Σύνολο:</label>
+								<div class="input-group">
+									<input type="password" class="form-control" placeholder="Σύνολο" value=15>
+									<span class="input-group-addon">
+										<i class="fa fa-euro"></i>
+									</span>
+								</div>
 							</div>
 						</div>
 						<div class="col-md-6">
