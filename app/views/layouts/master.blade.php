070a5087064d9697f3948468ec469689bd792d61
Slave <p.hadjichristodoulou@hotmail.com>
2014-07-30 16:37:16 +0200

    asd

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -657,25 +657,32 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
 						<span class="arrow">
 						</span>
 					</a>
-					<ul class="sub-menu" style="display: none;">
+					<li class="tooltips" data-container="body" data-placement="right" data-html="true" data-original-title="Συνολο προιόντων">
+							<a href="/azadmin/myproject/public/app/data/Product/view">
+								<span class="title">
+									Δημιουργία
+								</span>
+							</a>
+						</li>
+					<!-- <ul class="sub-menu" style="display: none;">
 						<li class="tooltips" data-container="body" data-placement="right" data-html="true" data-original-title="Δημιουργία χρήστη πλατφόρμας">
 							<a href="/azadmin/myproject/public/app/data/Order/create">
 								<span class="title">
 									Δημιουργία
 								</span>
 							</a>
-						</li>
-						<li class="tooltips" data-container="body" data-placement="right" data-html="true" data-original-title="Τροποποίηση/Διαγραφή χρήστη πλατφόρμας">
+						</li> -->
+						<!-- <li class="tooltips" data-container="body" data-placement="right" data-html="true" data-original-title="Τροποποίηση/Διαγραφή χρήστη πλατφόρμας">
 							<a href="/azadmin/myproject/public/app/data/Order/edit">
 								<span class="title">
 									Τροποποίηση
 								</span>
 							</a>
-						</li>
+						</li> -->
 						<li class="tooltips" data-container="body" data-placement="right" data-html="true" data-original-title="Δημιουργία χρήστη πλατφόρμας">
 							<a href="/azadmin/myproject/public/app/data/Order/display">
 								<span class="title">
-									Μεμονομένη παραγγελία
+									Τροποποίηση
 								</span>
 							</a>
 						</li>
@@ -735,13 +742,6 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
 							<a href="/azadmin/myproject/public/app/data/Product/edit">
 								<span class="title">
 									Τροποποίηση
-								</span>
-							</a>
-						</li>
-						<li class="tooltips" data-container="body" data-placement="right" data-html="true" data-original-title="Συνολο προιόντων">
-							<a href="/azadmin/myproject/public/app/data/Product/view">
-								<span class="title">
-									Συνολο προιόντων
 								</span>
 							</a>
 						</li>
@@ -825,7 +825,7 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
 <script src="/azadmin/assets/plugins/jquery-validation/dist/jquery.validate.min.js" type="text/javascript"></script>
 <script src="/azadmin/assets/plugins/backstretch/jquery.backstretch.min.js" type="text/javascript"></script>
 <script src="/azadmin/myproject/public/scripts/login-soft.js" type="text/javascript"></script>
-<script src="/myproject/public/scripts/customScripts.js" type="text/javascript"></script>
+<script src="/azadmin/myproject/public/scripts/customScripts.js" type="text/javascript"></script>
 <script src="/azadmin/assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
 <script src="/azadmin/assets/scripts/custom/components-pickers.js"></script>
 <!-- END LOGIN SCRIPTS -->
