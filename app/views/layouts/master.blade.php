2327a923b456c51c2bd0222bd4f0e1c6fe595e2c
Slave <p.hadjichristodoulou@hotmail.com>
2014-08-06 00:35:08 +0200

    master paths

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -181,292 +181,27 @@
 			</li>
 			<!-- END NOTIFICATION DROPDOWN -->
 			<!-- BEGIN INBOX DROPDOWN -->
-			<li class="dropdown" id="header_inbox_bar">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-				<i class="fa fa-envelope"></i>
-				<span class="badge">
-					 5
-				</span>
-				</a>
-				<ul class="dropdown-menu extended inbox">
-					<li>
-						<p>
-							 You have 12 new messages
-						</p>
-					</li>
-					<li>
-						<ul class="dropdown-menu-list scroller" style="height: 250px;">
-							<li>
-								<a href="inbox.html?a=view">
-								<span class="photo">
-									<img src="/azadmin/assets/img/avatar2.jpg" alt=""/>
-								</span>
-								<span class="subject">
-									<span class="from">
-										 Lisa Wong
-									</span>
-									<span class="time">
-										 Just Now
-									</span>
-								</span>
-								<span class="message">
-									 Vivamus sed auctor nibh congue nibh. auctor nibh auctor nibh...
-								</span>
-								</a>
-							</li>
-							<li>
-								<a href="inbox.html?a=view">
-								<span class="photo">
-									<img src="/azadmin/assets/img/avatar3.jpg" alt=""/>
-								</span>
-								<span class="subject">
-									<span class="from">
-										 Richard Doe
-									</span>
-									<span class="time">
-										 16 mins
-									</span>
-								</span>
-								<span class="message">
-									 Vivamus sed congue nibh auctor nibh congue nibh. auctor nibh auctor nibh...
-								</span>
-								</a>
-							</li>
-							<li>
-								<a href="inbox.html?a=view">
-								<span class="photo">
-									<img src="/azadmin/assets/img/avatar1.jpg" alt=""/>
-								</span>
-								<span class="subject">
-									<span class="from">
-										 Γιάννης Αποστόλου </span>
-									<span class="time">
-										 2 hrs
-									</span>
-								</span>
-								<span class="message">
-									 Vivamus sed nibh auctor nibh congue nibh. auctor nibh auctor nibh...
-								</span>
-								</a>
-							</li>
-							<li>
-								<a href="inbox.html?a=view">
-								<span class="photo">
-									<img src="/azadmin/assets/img/avatar2.jpg" alt=""/>
-								</span>
-								<span class="subject">
-									<span class="from">
-										 Lisa Wong
-									</span>
-									<span class="time">
-										 40 mins
-									</span>
-								</span>
-								<span class="message">
-									 Vivamus sed auctor 40% nibh congue nibh...
-								</span>
-								</a>
-							</li>
-							<li>
-								<a href="inbox.html?a=view">
-								<span class="photo">
-									<img src="/azadmin/assets/img/avatar3.jpg" alt=""/>
-								</span>
-								<span class="subject">
-									<span class="from">
-										 Richard Doe
-									</span>
-									<span class="time">
-										 46 mins
-									</span>
-								</span>
-								<span class="message">
-									 Vivamus sed congue nibh auctor nibh congue nibh. auctor nibh auctor nibh...
-								</span>
-								</a>
-							</li>
-						</ul>
-					</li>
-					<li class="external">
-						<a href="inbox.html">See all messages <i class="m-icon-swapright"></i></a>
-					</li>
-				</ul>
-			</li>
 			<!-- END INBOX DROPDOWN -->
 			<!-- BEGIN TODO DROPDOWN -->
-			<li class="dropdown" id="header_task_bar">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-				<i class="fa fa-tasks"></i>
-				<span class="badge">
-					 5
-				</span>
-				</a>
-				<ul class="dropdown-menu extended tasks">
-					<li>
-						<p>
-							 You have 12 pending tasks
-						</p>
-					</li>
-					<li>
-						<ul class="dropdown-menu-list scroller" style="height: 250px;">
-							<li>
-								<a href="#">
-								<span class="task">
-									<span class="desc">
-										 New release v1.2
-									</span>
-									<span class="percent">
-										 30%
-									</span>
-								</span>
-								<span class="progress">
-									<span style="width: 40%;" class="progress-bar progress-bar-success" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">
-										<span class="sr-only">
-											 40% Complete
-										</span>
-									</span>
-								</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-								<span class="task">
-									<span class="desc">
-										 Application deployment
-									</span>
-									<span class="percent">
-										 65%
-									</span>
-								</span>
-								<span class="progress progress-striped">
-									<span style="width: 65%;" class="progress-bar progress-bar-danger" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
-										<span class="sr-only">
-											 65% Complete
-										</span>
-									</span>
-								</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-								<span class="task">
-									<span class="desc">
-										 Mobile app release
-									</span>
-									<span class="percent">
-										 98%
-									</span>
-								</span>
-								<span class="progress">
-									<span style="width: 98%;" class="progress-bar progress-bar-success" aria-valuenow="98" aria-valuemin="0" aria-valuemax="100">
-										<span class="sr-only">
-											 98% Complete
-										</span>
-									</span>
-								</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-								<span class="task">
-									<span class="desc">
-										 Database migration
-									</span>
-									<span class="percent">
-										 10%
-									</span>
-								</span>
-								<span class="progress progress-striped">
-									<span style="width: 10%;" class="progress-bar progress-bar-warning" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
-										<span class="sr-only">
-											 10% Complete
-										</span>
-									</span>
-								</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-								<span class="task">
-									<span class="desc">
-										 Web server upgrade
-									</span>
-									<span class="percent">
-										 58%
-									</span>
-								</span>
-								<span class="progress progress-striped">
-									<span style="width: 58%;" class="progress-bar progress-bar-info" aria-valuenow="58" aria-valuemin="0" aria-valuemax="100">
-										<span class="sr-only">
-											 58% Complete
-										</span>
-									</span>
-								</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-								<span class="task">
-									<span class="desc">
-										 Mobile development
-									</span>
-									<span class="percent">
-										 85%
-									</span>
-								</span>
-								<span class="progress progress-striped">
-									<span style="width: 85%;" class="progress-bar progress-bar-success" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
-										<span class="sr-only">
-											 85% Complete
-										</span>
-									</span>
-								</span>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-								<span class="task">
-									<span class="desc">
-										 New UI release
-									</span>
-									<span class="percent">
-										 18%
-									</span>
-								</span>
-								<span class="progress progress-striped">
-									<span style="width: 18%;" class="progress-bar progress-bar-important" aria-valuenow="18" aria-valuemin="0" aria-valuemax="100">
-										<span class="sr-only">
-											 18% Complete
-										</span>
-									</span>
-								</span>
-								</a>
-							</li>
-						</ul>
-					</li>
-					<li class="external">
-						<a href="#">See all tasks <i class="m-icon-swapright"></i></a>
-					</li>
-				</ul>
-			</li>
+
 			<!-- END TODO DROPDOWN -->
 			<!-- BEGIN USER LOGIN DROPDOWN -->
 			<li class="dropdown user">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
 				<img alt="" src="/azadmin/assets/img/avatar1_small.jpg"/>
 				<span class="username">
-					 Γιάννης Αποστόλου
+					 {{Employee::find(Auth::user()->userId)->name}} {{Employee::find(Auth::user()->userId)->lname}}
 				</span>
 				<i class="fa fa-angle-down"></i>
 				</a>
 				<ul class="dropdown-menu">
 					<li>
-						<a href="extra_profile.html"><i class="fa fa-user"></i> My Profile</a>
+						<a href="/azadmin/myproject/public/app/user"><i class="fa fa-user"></i> My Profile</a>
 					</li>
 					<li>
-						<a href="page_calendar.html"><i class="fa fa-calendar"></i> My Calendar</a>
+						<a href="/azadmin/myproject/public/calendar/calendar"><i class="fa fa-calendar"></i> My Calendar</a>
 					</li>
-					<li>
+					<!-- <li>
 						<a href="inbox.html"><i class="fa fa-envelope"></i> My Inbox
 						<span class="badge badge-danger">
 							 3
@@ -479,14 +214,14 @@
 							 7
 						</span>
 						</a>
-					</li>
+					</li> -->
 					<li class="divider">
 					</li>
 					<li>
 						<a href="javascript:;" id="trigger_fullscreen"><i class="fa fa-move"></i> Full Screen</a>
 					</li>
 					<li>
-						<a href="extra_lock.html"><i class="fa fa-lock"></i> Lock Screen</a>
+						<a href="/myproject/public/app/lock"><i class="fa fa-lock"></i> Lock Screen</a>
 					</li>
 					<li>
 						<a href="/azadmin/myproject/public/auth/logout"><i class="fa fa-key"></i> Log Out</a>
@@ -818,10 +553,10 @@
 <script src="/azadmin/assets/plugins/jquery-validation/dist/jquery.validate.min.js" type="text/javascript"></script>
 <script src="/azadmin/assets/plugins/backstretch/jquery.backstretch.min.js" type="text/javascript"></script>
 <script src="/azadmin/myproject/public/scripts/login-soft.js" type="text/javascript"></script>
-<script src="/myproject/public/scripts/customScripts.js" type="text/javascript"></script>
+<script src="/azadmin/myproject/public/scripts/customScripts.js" type="text/javascript"></script>
 <script src="/azadmin/assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
-<script src="/myproject/public/components-pickers.js"></script>
-<script src="/myproject/public/components-dropdowns.js"></script>
+<script src="/azadmin/myproject/public/components-pickers.js"></script>
+<script src="/azadmin/myproject/public/components-dropdowns.js"></script>
 
 <!-- END LOGIN SCRIPTS -->
  
