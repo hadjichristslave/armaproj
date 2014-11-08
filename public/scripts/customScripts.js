bba5b57bcd8ea0f0b802b8de1e0f6e0d76d76125
Slave <p.hadjichristodoulou@hotmail.com>
2014-11-08 18:09:30 +0100

    fixes

--- public/scripts/customScripts.js
+++ public/scripts/customScripts.js
@@ -489,8 +489,8 @@ function updateProducts(productId){
 		}else console.log('did not exist');
 	if(flag == false){
 		prodQty      = 0;
-		prodName     = $.find('tr[rowId="'+productId+'"] td:nth-child(6)').text();
-		unitPrice    = $.find('tr[rowId="'+productId+'"] td:nth-child(8)').text();
+		prodName     = $.find('tr[rowId="'+productId+'"] td:nth-child(6)')[0].textContent;
+		unitPrice    = $.find('tr[rowId="'+productId+'"] td:nth-child(8)')[0].textContent;
 		itemCartProducts.push(new itemCart(productId, prodName, prodQty, unitPrice));
 	}
 	updateProductView();
